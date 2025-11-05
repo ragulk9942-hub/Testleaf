fc0b2941cf1bd7a38575fa67b108dc7f0d32fd46
ragulk9942-hub <ragulk9942@gmail.com>
2025-11-05 17:11:51 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,11 @@
+package assignment;
+
+public class MultiplicationTable {
+	 public static void main(String[] args) {
+	        int num = 8;
+	        for (int i = 1; i <= 10; i++) {
+	            System.out.println(num + " x " + i + " = " + (num * i));
+	        }
+	    }
+
+}
